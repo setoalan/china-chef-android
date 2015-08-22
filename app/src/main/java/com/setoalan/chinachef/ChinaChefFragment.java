416ffb178ee4cf4d498626deb6043ce70517194a
Alan Seto <aseto@umich.edu>
2015-08-21 23:21:10 -0500

    Functioning appetizer layout

--- app/src/main/java/com/setoalan/chinachef/ChinaChefFragment.java
+++ app/src/main/java/com/setoalan/chinachef/ChinaChefFragment.java
@@ -10,23 +10,46 @@ import android.widget.TextView;
 
 public class ChinaChefFragment extends Fragment {
 
+    private int quantity = 0;
+
     private TextView mNameTextView;
     private TextView mPriceTextView;
-    private TextView mDescriptionTextView;
-    private Button mDecreaseQuantityButton;
+    private Button mDecreaseButton;
     private TextView mQuantityTextView;
-    private Button mIncreaseQuantityTextView;
+    private Button mIncreaseTextView;
+    private TextView mDescriptionTextView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_china_chef, container, false);
 
         mNameTextView = (TextView) view.findViewById(R.id.appetizer_name_text_view);
+        mNameTextView.setText("Appetizer");
+
         mPriceTextView = (TextView) view.findViewById(R.id.appetizer_price_text_view);
-        mDescriptionTextView = (TextView) view.findViewById(R.id.appetizer_description_text_view);
-        mDecreaseQuantityButton = (Button) view.findViewById(R.id.decrease_quantity_button);
+        mPriceTextView.setText("$9.99");
+
+        mDecreaseButton = (Button) view.findViewById(R.id.decrease_quantity_button);
+        mDecreaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (quantity != 0) quantity--;
+                mQuantityTextView.setText(String.valueOf(quantity));
+            }
+        });
         mQuantityTextView = (TextView) view.findViewById(R.id.quantity_text_view);
-        mIncreaseQuantityTextView = (Button) view.findViewById(R.id.increase_quantity_button);
+        mQuantityTextView.setText(String.valueOf(quantity));
+
+        mIncreaseTextView = (Button) view.findViewById(R.id.increase_quantity_button);
+        mIncreaseTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mQuantityTextView.setText(String.valueOf(++quantity));
+            }
+        });
+
+        mDescriptionTextView = (TextView) view.findViewById(R.id.appetizer_description_text_view);
+        mDescriptionTextView.setText("Description of the appetizer");
 
         return view;
     }
